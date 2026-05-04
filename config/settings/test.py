d423262f2f14016858ef10e0c9091f89b3c1a649
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2026-05-04 11:18:23 +0100

    chore(config): remove sqlite test database override

--- config/settings/test.py
+++ config/settings/test.py
@@ -3,15 +3,7 @@
 from __future__ import annotations
 
 from config.settings.local import *  # noqa: F403
-from config.settings.local import BASE_DIR
 from config.settings.local import MIDDLEWARE as BASE_MIDDLEWARE
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "test.sqlite3",
-    }
-}
 
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.MD5PasswordHasher",
